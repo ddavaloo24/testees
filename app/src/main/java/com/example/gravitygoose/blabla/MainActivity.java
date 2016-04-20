36ce6adeb9852b5b8faa4cf671b90b5c73453642
robotgoat <cybercerebrum@outlook.com>
2016-04-20 00:46:41 -0700

    first

---
+++ app/src/main/java/com/example/gravitygoose/blabla/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gravitygoose.blabla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
